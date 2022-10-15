c2e367f260acb7e53bdd6ec3b4e032d9c3dbfee4
Denis <demaz@bk.ru>
2022-10-15 21:36:45 +0700

    Initial commit

---
+++ src/main/java/pro/sky/homeworks/homeworks2coursespring/Homeworks2CourseSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworks.homeworks2coursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworks2CourseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homeworks2CourseSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/homeworks/homeworks2coursespring/Homeworks2CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworks.homeworks2coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homeworks2CourseSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
